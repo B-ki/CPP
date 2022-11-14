a98760614ce235f7929d3937468b64cb9ac23969
B-ki <morel.romain81@gmail.com>
2022-11-14 16:39:55 +0200

    Mod2 ex02 done

---
+++ Mod02/ex02/srcs/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmorel <rmorel@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/11 15:44:36 by rmorel            #+#    #+#             */
+/*   Updated: 2022/11/14 16:29:09 by rmorel           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int main(void)
+{
+	Fixed a;
+	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+	Fixed const c( Fixed(5.05f) / Fixed ( 2 ));
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	std::cout << Fixed::max( a, b ) << std::endl;
+	std::cout << c << std::endl;
+	return 0;
+}
